1f9031df8862864de570dab8e8e16b57ecbafb80
icebear32 <1727693137@qq.com>
2024-01-03 15:05:57 +0800

    导航守卫-后置守卫

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -1,9 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
+import { createApp, createVNode, render } from 'vue'
+// import './style.css'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import loadingBar from './components/loadingBar.vue'
+
+// console.log(loadingBar)
+const Vnode = createVNode(loadingBar)
+render(Vnode, document.body)
+console.log(Vnode)
 
 const app = createApp(App)
 
@@ -11,12 +17,20 @@ app.use(router)
 app.use(ElementPlus)
 
 const whileList = ['/']
+
+// 前置守卫
 router.beforeEach((to, from, next) => {
+    Vnode.component?.exposed?.startLoading()
     if (whileList.includes(to.path) || localStorage.getItem('token')) {
         next()
     } else {
         next('/')
     }
+})
+
+// 后置守卫
+router.afterEach((to, from) => {
+    Vnode.component?.exposed?.endLoading()
 })
 
 app.mount('#app')
